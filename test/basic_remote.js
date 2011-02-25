7eaf2b2bf8420d2227f22eeb21c7f340c244ec98
Brian McDaniel <brianmcd05@gmail.com>
2011-02-25 13:49:08 -0500

    Updated test/basic_remote.js to work with newest API.

--- test/basic_remote.js
+++ test/basic_remote.js
@@ -1,20 +1,20 @@
 var manager = require('jsdom-manager');
 
-var inst = manager.lookup(1);
-inst.loadPage('http://news.ycombinator.com/', function () { 
-    var script = inst.document.createElement('script');
-    script.src = 'http://code.jquery.com/jquery-1.5.min.js';
+manager.lookup(1, function (inst) {
+    inst.loadPage('http://news.ycombinator.com/', function () { 
+        var script = inst.document.createElement('script');
+        script.src = 'http://code.jquery.com/jquery-1.5.min.js';
 
-    script.onload = function () {
-        console.log('HN Links');
-        var $ = inst.window.$;
-        $('td.title:not(:last) a').each(function() {
-            console.log(' -', $(this).text());
-        });
-    };
-    script.onerror = function () {
-        console.log('Error loading jQuery');
-    };
-    inst.document.documentElement.appendChild(script);
+        script.onload = function () {
+            console.log('HN Links');
+            var $ = inst.window.$;
+            $('td.title:not(:last) a').each(function() {
+                console.log(' -', $(this).text());
+            });
+        };
+        script.onerror = function () {
+            console.log('Error loading jQuery');
+        };
+        inst.document.documentElement.appendChild(script);
+    });
 });
-
