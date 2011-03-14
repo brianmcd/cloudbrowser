354b640a0ea17c0186b7fde0d211db170903fb49
Brian McDaniel <brianmcd05@gmail.com>
2011-03-14 19:44:11 -0400

    Added vt.Server class, which is an API for creating HTTP servers that uses BrowserInstances.
    vt.Server is in lib/server.js.  It is the highest level API for our
    library, and it manages a Connect server that uses BrowserInstances
    to load/render pages.  The user just needs to pass in a router function
    and some configuration parameters.  This will also be useful for
    programmatically creating a server for unit tests.
    
    Rewrote examples/test-server/server.js to use the new Server class.

--- examples/test-server/server.js
+++ examples/test-server/server.js
@@ -1,154 +1,56 @@
-var connect        = require('connect'),
-    fs             = require('fs'),
-    assert         = require('assert'),
-    BrowserManager = require('vt').BrowserManager;
-
 // I'm sure there's a better way to do this but not a priority now.
+// Try to get us into the right directory.
 if (process.cwd().match(/examples\/?$/)) {
     process.chdir('test-server');
 } else if (!process.cwd().match(/test-server\/?$/)) {
     process.chdir('examples/test-server');
 }
 
-var browsers = new BrowserManager();
-var basePage = fs.readFileSync('base.html', 'utf8'); //cache the base page
-
-var memory = new connect.session.MemoryStore({
-    reapInterval: 60000
-  , maxAge: 60000 * 2
+var Server = require('vt').Server;
+var server = new Server({
+    routes: app,
+    staticDir: __dirname + '/public',
+    basePage: './base.html'
 });
-
-var staticDir = __dirname + '/public';
-console.log('Static directory: ' + staticDir);
-
-var server = connect.createServer(
-    connect.logger(),
-    connect.cookieParser(),
-    connect.session({ store: memory, secret: 'test' }),
-    connect.static(staticDir),
-    connect.router(app)
-);
 
 server.listen(3000);
-console.log('Server listening on port 3000');
 
-//NOTE: When client connects to socket.io socket, then it has rendered the DOM and gotten to onLoad();
-var io = require('socket.io');
-var socket = io.listen(server);
-socket.on('connection', function (client) {
-    var browser = undefined;
-    var numMessages = 0;
-    //TODO: can use client.once
-    client.on('message', function (msg) {
-        if (browser == undefined) {
-            // msg should be the client's sessionID
-            console.log('Socket.io client connected: ' + msg);
-            assert.equal(numMessages, 0, "browser should only be undefined on first message");
-            browsers.lookup(msg, function (browse) {
-                browser = browse;
-                browser.initializeClient(client);
-            });
-        } else {
-            // otherwise, this was an event handler firing, but we haven't gotten there yet.
-            throw new Error("Client shouldn't be triggering events yet.");
-        }
-    });
-    client.on('disconnect', function (msg) {
-        console.log('Client disconnected.');
-        browser = undefined;
-    });
-});
-
-// These will be for initial calls to a new resource
-// TODO: add err as first param to callbacks
-function app(app) {
+function app (app) {
     app.get('/local/:filename', function (req, res) {
-        loadLocal({
+        server.loadLocal({
             req: req,
             success : function (browser) {
-                res.writeHead(200, {'Content-type': 'text/html'});
-                res.end(basePage.replace(/:SESSION_ID:/, req.sessionID));
+                server.returnBasePage(req.sessionID, res);
             },
-            failure : function () { send500error(res); }
+            failure : function () { server.send500error(res); }
         });
     });
     app.get('/localHTML/:filename', function (req, res) {
-        loadLocal({
+        server.loadLocal({
             req: req,
             success : function (browser) {
-                res.writeHead(200, {'Content-type': 'text/html'});
-                res.end(browser.dumpHTML());
+                server.returnHTML(browser, res);
             },
-            failure : function () { send500error(res); }
+            failure : function () { server.send500error(res); }
         });
     });
     app.get('/remote/:remoteURL', function (req, res) {
-        loadRemote({
+        server.loadRemote({
             req : req,
             success : function (browser) {
-                res.writeHead(200, {'Content-type': 'text/html'});
-                res.end(basePage.replace(/:SESSION_ID:/, req.sessionID));
+                server.returnBasePage(req.sessionID, res);
             },
-            failure : function () { send500error(res); }
+            failure : function () { server.send500error(res); }
         });
     });
     app.get('/remoteHTML/:remoteURL', function (req, res) {
-        loadRemote({
+        server.loadRemote({
             req : req,
             success : function (browser) {
-                res.writeHead(200, {'Content-type': 'text/html'});
-                res.end(browser.dumpHTML());
+                server.returnHTML(browser, res);
             },
-            failure : function () { send500error(res); }
+            failure : function () { server.send500error(res); }
         });
     });
 };
 
-function send500error (res) {
-    res.writeHead(500, {'Content-type': 'text/html'});
-    res.end();
-};
-
-function loadLocal (opts) {
-    var req = opts.req;
-    var filename = './' + req.params.filename; //TODO: don't allow /../
-    console.log('req.sessionID: ' + req.sessionID);
-    browsers.lookup(req.sessionID, function (browser) {
-        //NOTE: desktop.load will need to close the socket.io sockets (maybe)
-        //TODO: security...
-        browser.loadFromFile({
-            path : filename,
-            success : function () {
-                opts.success(browser);
-            },
-            failure : function () {
-                console.log('ERROR: failed to load requested file: ' + 
-                            filename);
-                opts.failure();
-            }
-        });
-    });
-};
-
-// req.params.remoteURL must be set before calling this function.
-function loadRemote (opts) {
-    var req = opts.req;
-    var remoteURL = req.params.remoteURL;
-    if (!remoteURL.match(/^http/)) {
-        remoteURL = 'http://' + remoteURL;
-    }
-    console.log('Remote URL requested: ' + remoteURL);
-    browsers.lookup(req.sessionID, function (browser) {
-        browser.loadFromURL({
-            url: remoteURL,
-            success : function () {
-                opts.success(browser);
-            },
-            failure : function () {
-                console.log('ERROR: failed to load remote URL: ' 
-                            + remoteURL);
-                opts.failure();
-            }
-        });
-    });
-}
--- vt.js
+++ vt.js
@@ -1,3 +1,4 @@
 exports.BrowserInstance = require('./lib/browser_instance');
 exports.BrowserManager  = require('./lib/browser_manager');
 exports.client          = require('./lib/client');
+exports.Server          = require('./lib/server');
