f86c2210e046d49870474277c4dbfe24c82776ef
Brian McDaniel <brianmcd05@gmail.com>
2011-04-07 01:10:16 -0400

    Added support for attribute methods on both client and server side.
    Also re-factored dom.js and fixed some bugs.

--- repl.js
+++ repl.js
@@ -1,9 +1,12 @@
 var repl = require('repl'),
+    path = require('path'),
     BI   = require('./lib/browser_instance');
 
 var b = new BI();
 var htmlpage = "<html><head><title>Title</title></head><body>This is the body</body></html>";
+var nodeinsert = path.join(__dirname, 'examples', 'test-server', 'nodeinsert', 'index.html');
 
 var repl = repl.start()
 repl.context.b = b;
 repl.context.htmlpage = htmlpage;
+repl.context.nodeinsert = nodeinsert;
