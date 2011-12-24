566c06b99b3b07859a2d7fe2037e0c063ba1771f
Brian McDaniel <brianmcd05@gmail.com>
2011-12-23 19:02:10 -0500

    [chat2] chat2 pretty much works, but needs refactorig.

--- examples/chat2/pages/home.js
+++ examples/chat2/pages/home.js
@@ -4,6 +4,11 @@
         newRoomName  : ko.observable('NewRoom'),
         selectedRoom : ko.observable(vt.shared.rooms()[0])
     };
+    viewModel.rooms.subscribe(function (val) {
+        if (val.length == 1) {
+            viewModel.selectedRoom(val[0]);
+        }
+    });
     ko.applyBindings(viewModel);
     $('#create-room').click(function () {
         var found = false;
@@ -35,6 +40,22 @@
         $('#chats-li').addClass('active');
         vt.pages.chats.load();
     });
+    // TODO: obviously refactor with above
     $('#join-room').click(function () {
+        var room = viewModel.selectedRoom();
+        local.chats.push(room);
+        local.activeRoom(room);
+        local.chatText[room.name] = ko.observableArray();
+        local.chatText[room.name]().toString = function () { return this.join('\n');};
+        // TODO: this could double register, need to see if it's already in local.chats.
+        room.on('newMessage', function (username, msg) {
+            local.chatText[room.name].push('[' + username + '] ' + msg);
+            if (room != local.activeRoom()) {
+                // TODO: turn its tab in chats red.
+            }
+        });
+        $('.topbar li').removeClass('active');
+        $('#chats-li').addClass('active');
+        vt.pages.chats.load();
     });
 })();
