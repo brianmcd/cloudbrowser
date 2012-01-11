e77ff2a697efadebf25e8dc2961e86349dc8218c
Brian McDaniel <brianmcd05@gmail.com>
2012-01-11 02:14:38 -0500

    vt-node-lib index.js exports Application class.

--- index.js
+++ index.js
@@ -2,5 +2,6 @@ require('coffee-script');
 exports.Server         = require('./src/server');
 exports.BrowserManager = require('./src/server/browser_manager');
 exports.Browser        = require('./src/server/browser');
+exports.Application    = require('./src/server/application')
 exports.Model          = require('./src/api/model')
 exports.ko             = require('./src/api/ko').ko
