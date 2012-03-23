ef7b770a5b5edffc417b2b5b3fe21080e63e5264
Brian McDaniel <brianmcd05@gmail.com>
2012-03-22 22:30:55 -0400

    Added some helper scripts.

---
+++ run_benchmark_app.sh
@@ -0,0 +1,1 @@
+node bin/server --compression=false -resource-proxy=false examples/benchmark-app/app.js
---
+++ run_hw.sh
@@ -0,0 +1,1 @@
+node --trace_gc --expose_gc bin/server --debug-server=true --admin-interface=true --compression=false --knockout --debug --resource-proxy=false --trace-mem examples/helloworld/app.js
