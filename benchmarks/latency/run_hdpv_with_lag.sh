459fd2f8afc69594799c075bb745b849741d88c2
Brian McDaniel <brianmcd05@gmail.com>
2012-04-11 12:29:01 -0400

    [benchmarks] latency: start at 1 client, plot linear fit with parameters.

--- benchmarks/latency/run_hdpv_with_lag.sh
+++ benchmarks/latency/run_hdpv_with_lag.sh
@@ -1,1 +1,1 @@
-node run --start-clients=0 --end-clients=10 --step-size=5 --server-address="http://hdpv.cs.vt.edu:3000" --ssh-host="hdpv.cs.vt.edu" --ssh-cmd="cd ~/cloudbrowser/benchmarks/framework/apps/benchmark && ./run_with_lag.sh"
+node run --start-clients=0 --end-clients=1000 --step-size=100 --server-address="http://hdpv.cs.vt.edu:3000" --ssh-host="hdpv.cs.vt.edu" --ssh-cmd="cd ~/cloudbrowser/benchmarks/framework/apps/benchmark && ./run_with_lag.sh"
