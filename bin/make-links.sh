4b1b01449272665ad8b3cada6b67f09bf9abaf58
Brian McDaniel <brianmcd05@gmail.com>
2012-05-10 17:38:33 -0400

    Working toward npm publish.

---
+++ bin/make-links.sh
@@ -0,0 +1,4 @@
+ln -s jsdom node_modules/jsdom-nocache
+ln -s ../../node_modules/knockout-node/build/output/knockout-node-latest.debug.js src/api/ko.js
+ln -s ../../../../node_modules/knockout-node/build/output/knockout-latest.debug.js src/server/browser/knockout/
+ln -s ../shared/ src/client/
