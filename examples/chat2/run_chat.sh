314201a7afdd54c2c11f02e2fcbb0279df786513
Brian McDaniel <brianmcd05@gmail.com>
2012-05-24 20:38:42 -0400

    run_chat.sh: clean up options.

--- examples/chat2/run_chat.sh
+++ examples/chat2/run_chat.sh
@@ -1,1 +1,1 @@
-node --debug --trace_gc --expose_gc ../../bin/server --admin-interface=true --compression=false --knockout --debug --resource-proxy=false --trace-mem app.js
+node ../../bin/server --admin-interface=true --knockout --debug app.js
