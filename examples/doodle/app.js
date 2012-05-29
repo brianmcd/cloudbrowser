bbd0db7c5a7c728b48565c5ed5921ba1b6deb34b
Brian McDaniel <brianmcd05@gmail.com>
2012-05-29 00:01:42 -0400

    [doodle] update to use new API.

--- examples/doodle/app.js
+++ examples/doodle/app.js
@@ -1,5 +1,10 @@
-var Path = require('path');
-exports.app = {
-  entryPoint  : Path.resolve(__dirname, 'index.html'),
-  mountPoint  : '/',
-};
+var Path = require('path'),
+    CloudBrowser = require('../../');
+
+var server = CloudBrowser.createServer({
+    knockout: true,
+    defaultApp: CloudBrowser.createApplication({
+        entryPoint  : Path.resolve(__dirname, 'index.html'),
+        mountPoint  : '/'
+    })
+});
