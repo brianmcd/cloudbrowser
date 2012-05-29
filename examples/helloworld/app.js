5b35f4b14a03eb8f4360ddf3b37e1f34e4df381f
Brian McDaniel <brianmcd05@gmail.com>
2012-05-29 00:04:05 -0400

    [helloworld] update to use new API.

--- examples/helloworld/app.js
+++ examples/helloworld/app.js
@@ -1,5 +1,8 @@
-var Path = require('path');
-exports.app = {
-  entryPoint  : Path.resolve(__dirname, 'index.html'),
-  mountPoint  : '/',
-};
+var Path = require('path'),
+    CloudBrowser = require('../../');
+
+var server = CloudBrowser.createServer();
+server.mount(CloudBrowser.createApplication({
+    entryPoint  : Path.resolve(__dirname, 'index.html'),
+    mountPoint  : '/'
+}));
