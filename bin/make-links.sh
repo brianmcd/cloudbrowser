1c617aa06095fbbcb37f4253ff3ca793e0500a25
Ashima Athri <ashimaathri@gmail.com>
2012-12-14 17:35:43 -0500

    Making JSDOM a direct dependency and adding postinstall/postupdate hooks to patch JSDOM for cloudbrowser use

--- bin/make-links.sh
+++ bin/make-links.sh
@@ -1,4 +1,3 @@
-ln -sf jsdom node_modules/jsdom-nocache
 ln -sf ../../node_modules/knockout-node/build/output/knockout-node-latest.debug.js src/api/ko.js
 ln -sf ../../../../node_modules/knockout-node/build/output/knockout-latest.debug.js src/server/browser/knockout/
 ln -sf ../shared/ src/client/
