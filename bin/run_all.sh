18ccb3323d24c8126e969986bef10b77297b2bfd
pan <pan@rnikola.cs.vt.edu>
2014-09-10 13:14:26 -0400

    add nohup for script

--- bin/run_all.sh
+++ bin/run_all.sh
@@ -3,7 +3,7 @@
 i_scriptDir=$(cd "$(dirname "$0")"; pwd)
 cd $i_scriptDir/..
 
-bin/run_master.sh examples src/server/applications --disable-logging=false &
+nohup bin/run_master.sh examples src/server/applications --disable-logging=false >master.log  2>master.err.log &
 
 i_workers=2
 if [[ "X$1" != "X" ]]; then
@@ -12,5 +12,5 @@ fi
 
 for (( i = 1; i <= $i_workers; i++ )); do
     echo start worker$i
-    bin/run_worker.sh --configPath config/worker$i &
+    nohup bin/run_worker.sh --configPath config/worker$i >worker$i.log 2>worker$i.err.log &
 done
