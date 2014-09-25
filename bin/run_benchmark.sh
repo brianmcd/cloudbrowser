9328b77a0c459cfcfc2b423e3b144c66b8f143d3
Blade Pan <blade@msn.cn>
2014-09-24 23:26:07 -0400

    add option to specify batch size

--- bin/run_benchmark.sh
+++ bin/run_benchmark.sh
@@ -5,6 +5,11 @@ cd $i_scriptDir/..
 i_time=$(date "+%Y%m%d%H%M%S")
 
 source $i_scriptDir/debug_env.sh
+
+if [[ "X$CB_BATCHSIZE" = "X" ]]; then
+    export CB_BATCHSIZE=20
+fi
+
 # print all the env variables set for test
 env | grep CB
 
@@ -18,13 +23,15 @@ if [[ "X$2" != "X" ]]; then
     i_prefix=$2
 fi
 
+
 echo start $i_logprefix with $i_processes benchmark processes
 
 for (( i = 0; i < $i_processes; i++ )); do
     i_group=p"$i"
     i_logfile="$i_prefix"_client_"$i_group"".log"
     nohup node_modules/coffee-script/bin/coffee benchmarks/clients/client_process.coffee \
-    --appinstance-count $CB_APPINS --browser-count $CB_BROWSER --client-count $CB_CLIENT --server-logging false \
+    --appinstance-count $CB_APPINS --browser-count $CB_BROWSER --client-count $CB_CLIENT \
+    --server-logging false --batch-size $CB_BATCHSIZE \
     --configFile $CB_CONF --app-address $CBAPP_ADDR --process-id $i_group >$i_logfile 2>&1 &
     echo benchmark process $i_group log to $i_logfile
 done
