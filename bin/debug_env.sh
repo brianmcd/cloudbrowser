960377f40f3b8d17adb82fc13e4b9eeec3835d30
Blade Pan <blade@msn.cn>
2014-11-30 22:02:16 -0500

    add kill script

--- bin/debug_env.sh
+++ bin/debug_env.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 if [[ "X$DEBUG" == "X" ]]; then
     export DEBUG=cloudbrowser:*,-cloudbrowser:master:proxy,-cloudbrowser:master:app,\
--cloudbrowser:worker:browser:*,-cloudbrowser:worker:init,-cloudbrowser:worker:dom:*
+-cloudbrowser:worker:browser,-cloudbrowser:worker:browser:*,-cloudbrowser:worker:init,\
+-cloudbrowser:worker:dom:*
 fi
 
 echo export DEBUG=$DEBUG
