a95494121b0150f7372850a9f7d58dd5b81e9e2c
Blade Pan <blade@msn.cn>
2014-12-11 17:32:43 -0500

    fix chat4 buildjs.sh

--- benchmarks/applications/chat4/buildjs.sh
+++ benchmarks/applications/chat4/buildjs.sh
@@ -2,7 +2,7 @@
 i_scriptDir=$(cd "$(dirname "$0")"; pwd)
 
 cd $i_scriptDir
-i_root_dir=$(cd ../../; pwd) 
+i_root_dir=$(cd ../../../; pwd) 
 
 i_coffee=$i_root_dir/node_modules/coffee-script/bin/coffee
 
