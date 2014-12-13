4537b8da06ac4c6cf1cf4172501eb17e8bdbd114
Blade Pan <blade@msn.cn>
2014-12-12 23:37:28 -0500

    add kill flag

--- bin/kill_all.sh
+++ bin/kill_all.sh
@@ -2,8 +2,17 @@
 i_scriptDir=$(cd "$(dirname "$0")"; pwd)
 cd $i_scriptDir/..
 
-for i in $(ps ax|grep node| grep -e newbin -e master| grep -v grep | awk '{split($0,a," "); print a[1]}'); do
-    echo kill $i
+# the processes are started by coffee, be careful here.
+# we only want to send signal to the actual node processes
+i_node=$(which node)
+
+i_flag=""
+if [[ "X$1" != "X" ]]; then
+    i_flag="-"$1
+fi
+
+for i in $(ps ax|grep $i_node| grep -e newbin -e master| grep -v grep | awk '{split($0,a," "); print a[1]}'); do
+    echo kill $i_flag $i
     kill $i
 done
 
