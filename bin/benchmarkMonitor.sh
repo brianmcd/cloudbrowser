1b206e535b18f341a336dccc10076aaa6ec90dc1
Blade Pan <blade@msn.cn>
2015-01-02 17:24:33 -0500

    add timeout check for benchmark monitor

--- bin/benchmarkMonitor.sh
+++ bin/benchmarkMonitor.sh
@@ -2,7 +2,16 @@
 i_scriptDir=$(cd "$(dirname "$0")"; pwd)
 cd $i_scriptDir/..
 
+i_totalTime=0
 i_interval=30
+# default limit 2h
+i_limit=$((3600*2))
+
+if [[ "X$1" != "X" ]]; then
+    i_limit=$1
+fi
+
+echo "watch client process for $i_limit"s
 
 while [[ true ]]; do
     i_nodes=$(ps ax | grep node |grep client | grep -v grep)
@@ -10,9 +19,18 @@ while [[ true ]]; do
         echo $(date) "No nodes program running. Exiting..."
         break
     else
-        echo $(date) "Node program running"
+        echo $(date) "Node program running. TotalTime $i_totalTime"s
+        if [[ $i_totalTime -gt $i_limit ]]; then
+            echo "timeout"
+            for i in $(ps ax | grep node |grep client | grep -v grep| awk '{split($0,a," "); print a[1]}'); do
+                echo "kill $i"
+                kill $i
+            done
+            continue
+        fi
     fi
     sleep $i_interval
+    i_totalTime=$((i_totalTime+i_interval))
 done
 
 
