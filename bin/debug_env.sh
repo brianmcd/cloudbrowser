639f2686253e8e6a164b0cb28c923919654902b4
Blade Pan <blade@msn.cn>
2015-02-03 09:30:52 -0500

    update debug config

--- bin/debug_env.sh
+++ bin/debug_env.sh
@@ -2,7 +2,7 @@
 if [[ "X$DEBUG" == "X" ]]; then
     export DEBUG=cloudbrowser:*,-cloudbrowser:master:proxy,-cloudbrowser:master:app,\
 -cloudbrowser:worker:browser,-cloudbrowser:worker:browser:*,-cloudbrowser:worker:init,\
--cloudbrowser:worker:dom:*
+-cloudbrowser:worker:dom:*,-cloudbrowser:worker:socket
 fi
 
 echo export DEBUG=$DEBUG
