e707f567915c078b65e925fddbe7c5b2a660b163
Blade Pan <blade@msn.cn>
2015-02-08 13:48:05 -0500

    generate random port for debug

--- bin/run_all.sh
+++ bin/run_all.sh
@@ -28,14 +28,29 @@ if [[ "X$CB_OPTS" == "X" ]]; then
     export CB_OPTS="--disable-logging=false"
 fi
 
+i_debugopts=""
+getDebugOpts(){
+    i_debugopts=""
+    if [[ "X$CB_DEBUG" == "Xtrue" ]]; then
+        # help me random number god
+        i_debugport=$(jot -r 1  9000 65000)
+        echo "debug worker at $i_debugport"
+        i_debugopts="--debug=$i_debugport"
+    fi
+}
+
 i_master_log=$i_prefix"_master.log"
+getDebugOpts
+export CB_MASTER_DEBUG_OPTS=$i_debugopts
 
 nohup bin/run_master.sh $i_apps $CB_OPTS >$i_master_log  2>&1 &
-echo master log write to $i_master_log
+echo master log write to $i_master_log , debug setting $CB_MASTER_DEBUG_OPTS
 
 
 for (( i = 1; i <= $i_workers; i++ )); do
     i_worker_log="$i_prefix"_worker"$i".log
+    getDebugOpts
+    export CB_WORKER_DEBUG_OPTS=$i_debugopts
     nohup bin/run_worker.sh --configPath config/worker$i >$i_worker_log 2>&1 &
-    echo worker$i log write to $i_worker_log
+    echo worker$i log write to $i_worker_log , debug setting $CB_WORKER_DEBUG_OPTS
 done
--- bin/run_worker.sh
+++ bin/run_worker.sh
@@ -3,5 +3,14 @@
 i_scriptDir=$(cd "$(dirname "$0")"; pwd)
 source $i_scriptDir/node_env.sh
 cd $i_scriptDir/..
-echo "starting worker with $@"
-$i_coffee --nodejs "$CB_NODE_ARGS" src/server/newbin.coffee $@
+
+
+i_node_args=$CB_NODE_ARGS
+
+if [[ "X$CB_WORKER_DEBUG_OPTS" != "X" ]]; then
+    i_node_args="$CB_NODE_ARGS $CB_WORKER_DEBUG_OPTS"
+fi
+
+echo "starting worker with $@, node parameter $i_node_args"
+
+$i_coffee --nodejs "$i_node_args" src/server/newbin.coffee $@
